5ae6ac4d0336849e96ad586c7628c2eeb0ed5666
Luis <luiscarlos.sf@outlook.com>
2019-05-19 15:57:49 -0300

    Muda a estrutura da programa

--- kdmer.py
+++ kdmer.py
@@ -5,15 +5,23 @@ d = 1
 sequence = TAATGCCATGGGATGTT
 Saída = 
 """
-import io
 
-k = int(input("k="))
-d = int(input("d="))
-sequence = input("Sequência de Nucleotídeos: ")
+def kdMer(sequence, k, d):
 
-with open("k"+str(k)+"d"+str(d)+"mer.txt", "w") as file:
-    tam =len(sequence)
-    kdmers = [sequence[i:(i+k)]+"|"+sequence[(i+k+d):(i+d+k+k)] for i in range(0, tam-(k+d+(k-1)))]
-    kdmers.sort()
-    string = str(kdmers)
-    file.write(string)
+    with open("k"+str(k)+"d"+str(d)+"mer.txt", "w") as file:
+        tam =len(sequence)
+        kdmers = [sequence[i:(i+k)]+"|"+sequence[(i+k+d):(i+d+k+k)] for i in range(0, tam-(k+d+(k-1)))]
+        kdmers.sort()
+        file.write('[')
+        for i, kdmer in enumerate(kdmers):
+            if i != 0:
+                file.write(',')
+            file.write(kdmer)
+        file.write(']')
+        return kdmers
+
+if __name__ == "__main__":
+    k = int(input("k="))
+    d = int(input("d="))
+    sequence = input("Sequência de Nucleotídeos: ")
+    kdMer(sequence, k, d)
